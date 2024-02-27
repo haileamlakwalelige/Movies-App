c5c963aee29dc2682ce9e480a692e4d4c3cb66a4
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-02-27 21:15:54 +0300

    update trending movies

--- components/components/TrendingMovies.js
+++ components/components/TrendingMovies.js
@@ -5,39 +5,43 @@ import { useNavigation } from '@react-navigation/native';
 
 var {width, height}=Dimensions.get('window');
 
-const TrendingMovies = ({data}) => {
+const TrendingMovies = ({ data }) => {
+  const navigation = useNavigation();
 
-    const navigation = useNavigation();
-
-    const handleClick=()=>{
-        navigation.navigate("Movie", item);
-    }
+  const handleClick = (item) => {
+    navigation.navigate("Movie", item);
+  };
 
   return (
     <View className="mb-8 mt-8">
       <Text className="text-white text-xl mx-4 mb-5 font-bold">Trending</Text>
       <Carousel
-      data={data}
-      renderItem={({item})=><MovieCard item={item} handleClick={handleClick}/>}
-      firstItem={1}
-      inactiveSlideOpacity={0.60}
-      sliderWidth={width}
-      itemWidth={width*0.62}
-      slideStyle={{display:'flex', alignItems:'center'}} />
+        data={data}
+        renderItem={({ item }) => (
+          <MovieCard item={item} handleClick={handleClick} />
+        )}
+        firstItem={1}
+        inactiveSlideOpacity={0.6}
+        sliderWidth={width}
+        itemWidth={width * 0.62}
+        slideStyle={{ display: "flex", alignItems: "center" }}
+      />
     </View>
-  )
-}
+  );
+};
 
-
-const MovieCard=({item})=>{
-    return(
-        <TouchableWithoutFeedback>
-            <Image source={require('../../assets/movie1.jpeg')} style={{width:width*0.6,
-            height:height*0.4}} className="rounded-3xl"/>
-            {/* <Text className="text-white">Movies</Text> */}
-        </TouchableWithoutFeedback>
-    )
-}
+const MovieCard = ({ item, handleClick }) => {
+  return (
+    <TouchableWithoutFeedback onPress={() => handleClick(item)}>
+      <Image
+        source={require("../../assets/movie1.jpeg")}
+        style={{ width: width * 0.6, height: height * 0.4 }}
+        className="rounded-3xl"
+      />
+      {/* <Text className="text-white">Movies</Text> */}
+    </TouchableWithoutFeedback>
+  );
+};
 
 export default TrendingMovies
 
