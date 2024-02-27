ffb056b060da8eb65893fa2a2058d610c26ea87e
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-02-27 21:16:44 +0300

    update app navigation

--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from '../components/screens/HomeScreen';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../components/screens/HomeScreen";
+import MovieScreen from "../components/screens/MovieScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -9,7 +10,16 @@ function AppNavigation() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" options={{ headerShown: false }} component={HomeScreen} />
+        <Stack.Screen
+          name="Home"
+          options={{ headerShown: false }}
+          component={HomeScreen}
+        />
+        <Stack.Screen
+          name="Movie"
+          options={{ headerShown: false }}
+          component={MovieScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
