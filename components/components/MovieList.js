1cd2644f948727a778f280850772da6df775d9ce
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-02-27 21:16:07 +0300

    Movies list update

---
+++ components/components/MovieList.js
@@ -0,0 +1,59 @@
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  TouchableWithoutFeedback,
+  Image,
+  Dimensions,
+} from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+
+var { width, height } = Dimensions.get("window");
+
+const MovieList = ({ title, data }) => {
+  let movieName = "Ant-Man and the Wasp: Quantumania";
+  const navigation = useNavigation();
+  return (
+    <View className="mb-8 space-y-4">
+      <View className="mx-4 flex-row justify-between items-center">
+        <Text className="text-white text-xl">{title}</Text>
+        <TouchableOpacity>
+          <Text className="text-[#eab308] text-lg">See All</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Movie Row */}
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 15 }}
+      >
+        {data.map((item, index) => {
+          return (
+            <TouchableWithoutFeedback
+              key={index}
+              onPress={() => navigation.navigate("Movie", item)}
+            >
+              <View className="space-y-1 mr-4">
+                <Image
+                  source={require("../../assets/movie2.jpeg")}
+                  className="rounded-xl"
+                  style={{ width: width * 0.33, height: height * 0.22 }}
+                />
+                <Text className="text-neutral-300 ml-1">
+                  {movieName.length > 14
+                    ? movieName.slice(0, 14) + "..."
+                    : movieName}
+                </Text>
+              </View>
+            </TouchableWithoutFeedback>
+          );
+        })}
+      </ScrollView>
+    </View>
+  );
+};
+
+export default MovieList;
